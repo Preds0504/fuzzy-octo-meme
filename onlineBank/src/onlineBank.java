3fbc1c6acf152fc29df4cf114b2721c48f518dd5
Tyler Hardison <tmhardis@ncsu.edu>
2023-01-27 16:27:06 -0500

    readme file

---
+++ onlineBank/src/onlineBank.java
@@ -0,0 +1,16 @@
+
+/**
+ * The class will take an input of side lengths and return the area of the triangle
+ * @author Tyler Hardison
+ * @author Javion May
+ */
+ 
+public class onlineBank {
+/**
+ * Starts the program
+ *
+ * @param args online bank
+ */
+    public static void main(String[] args) {
+    }
+}
