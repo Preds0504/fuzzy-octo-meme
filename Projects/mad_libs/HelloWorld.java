1f5b89b9963cb091cff2f7500f0b2cdaedb0f442
Tyler Hardison <tmhardis@ncsu.edu>
2023-01-27 16:19:44 -0500

    started the folder system

--- Projects/mad_libs/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
-*Thsisisiisi
-*@author Tyler Hardison
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Tyler Hardison!");
-    }
-}
